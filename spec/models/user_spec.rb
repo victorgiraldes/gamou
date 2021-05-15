52510f65d8cc78e6e5fb9328872f75d9d88ed29e
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-05-15 12:47:35 -0300

    add spec validate new user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validate new user' do
+    it 'should create a valid user' do
+      user = User.new(email: 'test@example.com', password: '123456')
+      expect(user).to be_valid
+    end 
+    it 'should create a user without email and its be invalid user' do
+      user = User.new(password: '123456')
+      expect(user).to_not be_valid
+    end 
+    it 'should create a user without password and its be invalid user' do
+      user = User.new(email: 'test@example.com')
+      expect(user).to_not be_valid
+    end 
+  end
 end
