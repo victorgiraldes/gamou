3a6169194dae5ba559143547862b821b24194862
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-05-20 20:46:23 -0300

    update home page

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'home/index.html.erb', type: :view do
-  it 'Gamification heading should be in the document' do
+  it 'Home should be in the document"' do
     render
 
-    expect(rendered).to match /Gamification.../
+    expect(rendered).to match /id="home"/
   end
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,30 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueGray: {
+          50: '#f8fafc',
+          200: '#e2e8f0',
+          300: '#475569',
+          400: '#94a3b8',
+          500: '#64748b',
+          600: '#475569',
+          700: '#334155',
+          800: '#1e293b'
+        },
+        lightBlue: {
+          400: '#38bdf8',
+          600: '#0384c7'
+        },
+        emerald: {
+          400: '#5dd499'
+        }
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
