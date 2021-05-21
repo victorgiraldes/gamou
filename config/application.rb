b3a7b673b4cbdb2f1785ed274ff8269c967f807d
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-05-21 19:00:58 -0300

    config timezone and I18n

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,25 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+def set_locate_configs_and_timezone
+  config.time_zone = 'Brasilia'
+  config.i18n.load_path +=
+    Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+  config.i18n.available_locales = ['pt-BR']
+  config.i18n.default_locale = 'pt-BR'
+end
 
 module Gamification
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    set_locate_configs_and_timezone
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
