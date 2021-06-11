bc0256695120eda7b69396b75f1ff7fd275f1871
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-06-11 17:08:40 -0300

    Fix translations missing

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,9 @@ def set_locate_configs_and_timezone
   config.time_zone = 'Brasilia'
   config.i18n.load_path +=
     Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
-  config.i18n.available_locales = ['pt-BR']
-  config.i18n.default_locale = 'pt-BR'
-end
+    config.i18n.available_locales = ['pt-BR', 'en']
+    config.i18n.default_locale = 'en'
+  end
 
 module Gamou
   class Application < Rails::Application
