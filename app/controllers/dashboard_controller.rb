6159e66bba4f54630351b05e40732f7a4324d717
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-06-27 02:21:12 -0300

    Add Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @today_sales = 530
+    @today_diamonds = 315
+    @today_products = 7
+    @today_new_customers = 25
+    @today_returning_customers = 48
+    @today_customers = @today_returning_customers + @today_new_customers
   end
 end
