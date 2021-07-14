9f34edf0046233138aab3e8c5ee28175a2d45957
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-07-14 20:26:13 -0300

    Add support to Rails Admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails
   .draw do
     scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
       root 'home#index'
+      mount RailsAdmin::Engine => 'admin', :as => 'rails_admin'
       devise_for :users, controllers: { sessions: 'users/sessions' }
 
+      get 'admin', to: 'admin/dashboard#index'
       get 'dashboard', to: 'dashboard#index'
-
     end
   end
