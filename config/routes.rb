a440093a59f9fc4b1d6d4fe8f59d16006fcdec5b
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-08-07 19:39:49 -0300

    Add registration user edit view

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails
     scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
       root 'home#index'
       mount RailsAdmin::Engine => 'admin', :as => 'rails_admin'
-      devise_for :users, controllers: { sessions: 'users/sessions' }
+      devise_for :users,
+                 controllers: {
+                   sessions: 'users/sessions',
+                   registrations: 'users/registrations'
+                 }
 
       get 'admin', to: 'admin/dashboard#index'
       get 'dashboard', to: 'dashboard#index'
